91db91cf9251c4a1211f082979dbde43958dd380
GitHub Action <action@github.com>
2020-10-03 04:02:37 +0000

    style: isort/black

--- src/task/pipeline.py
+++ src/task/pipeline.py
@@ -1,10 +1,12 @@
 import pickle
+from typing import Callable, Optional
 
 from omegaconf import DictConfig
-from typing import Optional, Callable
-from torch.utils.data import DataLoader, Dataset
 from pytorch_lightning import LightningDataModule
+from torch.utils.data import DataLoader, Dataset
+
 from ..data import MTATDataset
+
 
 class DataPipeline(LightningDataModule):
     def __init__(self, pipline_config: DictConfig) -> None:
@@ -15,55 +17,74 @@ class DataPipeline(LightningDataModule):
     def setup(self, stage: Optional[str] = None):
         if stage == "fit" or stage is None:
             self.train_dataset = DataPipeline.get_dataset(
-                                                        self.dataset_builder,
-                                                        self.pipeline_config.dataset.path,
-                                                        "TRAIN",
-                                                        self.pipeline_config.dataset.input_length
-                                                        )
+                self.dataset_builder,
+                self.pipeline_config.dataset.path,
+                "TRAIN",
+                self.pipeline_config.dataset.input_length,
+            )
 
-            self.val_dataset = DataPipeline.get_dataset(self.dataset_builder,
-                                                        self.pipeline_config.dataset.path,
-                                                        "VALID",
-                                                        self.pipeline_config.dataset.input_length)
+            self.val_dataset = DataPipeline.get_dataset(
+                self.dataset_builder,
+                self.pipeline_config.dataset.path,
+                "VALID",
+                self.pipeline_config.dataset.input_length,
+            )
 
         if stage == "test" or stage is None:
-            self.test_dataset = DataPipeline.get_dataset(self.dataset_builder,
-                                                        self.pipeline_config.dataset.path,
-                                                        "TEST",
-                                                        self.pipeline_config.dataset.input_length)
+            self.test_dataset = DataPipeline.get_dataset(
+                self.dataset_builder,
+                self.pipeline_config.dataset.path,
+                "TEST",
+                self.pipeline_config.dataset.input_length,
+            )
 
     def train_dataloader(self) -> DataLoader:
-        return DataPipeline.get_dataloader(self.train_dataset,
-                                           batch_size=self.pipeline_config.dataloader.params.batch_size,
-                                           num_workers=self.pipeline_config.dataloader.params.num_workers,
-                                           drop_last=True,
-                                           shuffle=True)
+        return DataPipeline.get_dataloader(
+            self.train_dataset,
+            batch_size=self.pipeline_config.dataloader.params.batch_size,
+            num_workers=self.pipeline_config.dataloader.params.num_workers,
+            drop_last=True,
+            shuffle=True,
+        )
 
     def val_dataloader(self) -> DataLoader:
-        return DataPipeline.get_dataloader(self.val_dataset,
-                                           batch_size=self.pipeline_config.dataloader.params.batch_size,
-                                           num_workers=self.pipeline_config.dataloader.params.num_workers,
-                                           drop_last=True,
-                                           shuffle=False)
+        return DataPipeline.get_dataloader(
+            self.val_dataset,
+            batch_size=self.pipeline_config.dataloader.params.batch_size,
+            num_workers=self.pipeline_config.dataloader.params.num_workers,
+            drop_last=True,
+            shuffle=False,
+        )
 
     def test_dataloader(self) -> DataLoader:
-        return DataPipeline.get_dataloader(self.test_dataset,
-                                           batch_size=self.pipeline_config.dataloader.params.batch_size,
-                                           num_workers=self.pipeline_config.dataloader.params.num_workers,
-                                           drop_last=True,
-                                           shuffle=False)
+        return DataPipeline.get_dataloader(
+            self.test_dataset,
+            batch_size=self.pipeline_config.dataloader.params.batch_size,
+            num_workers=self.pipeline_config.dataloader.params.num_workers,
+            drop_last=True,
+            shuffle=False,
+        )
 
     @classmethod
-    def get_dataset(cls, dataset_builder:Callable, root, split, length) -> Dataset:
+    def get_dataset(cls, dataset_builder: Callable, root, split, length) -> Dataset:
         dataset = dataset_builder(root, split, length)
         return dataset
 
     @classmethod
-    def get_dataloader(cls, dataset: Dataset, batch_size: int, num_workers: int, shuffle: bool, drop_last: bool,
-                       **kwargs) -> DataLoader:
-        return DataLoader(dataset,
-                          batch_size=batch_size,
-                          num_workers=num_workers,
-                          shuffle=shuffle,
-                          drop_last=drop_last,
-                          **kwargs)
+    def get_dataloader(
+        cls,
+        dataset: Dataset,
+        batch_size: int,
+        num_workers: int,
+        shuffle: bool,
+        drop_last: bool,
+        **kwargs
+    ) -> DataLoader:
+        return DataLoader(
+            dataset,
+            batch_size=batch_size,
+            num_workers=num_workers,
+            shuffle=shuffle,
+            drop_last=drop_last,
+            **kwargs
+        )
--- src/task/runner.py
+++ src/task/runner.py
@@ -1,12 +1,12 @@
 import torch
 import torch.nn as nn
-
 from omegaconf import DictConfig
+from pytorch_lightning import EvalResult, LightningModule, TrainResult
 from torch.optim import SGD
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from pytorch_lightning import LightningModule, EvalResult, TrainResult
 
 from ..metric import get_auc
+
 
 class AutotaggingRunner(LightningModule):
     def __init__(self, model: nn.Module, runner_config: DictConfig):
@@ -16,29 +16,28 @@ class AutotaggingRunner(LightningModule):
         self.hparams.update(runner_config.optimizer.params)
         self.hparams.update(runner_config.scheduler.params)
         self.hparams.update(runner_config.trainer.params)
-    
+
     def forward(self, x):
         return self.model(x)
 
     def configure_optimizers(self):
         opt = SGD(
-            self.model.parameters(), 
+            self.model.parameters(),
             lr=self.hparams.learning_rate,
-            momentum= self.hparams.momentum,
-            weight_decay= self.hparams.weight_decay,
-            nesterov= self.hparams.nesterov
+            momentum=self.hparams.momentum,
+            weight_decay=self.hparams.weight_decay,
+            nesterov=self.hparams.nesterov,
         )
 
         scheduler = ReduceLROnPlateau(
             optimizer=opt,
-            factor= self.hparams.factor,
-            patience= self.hparams.patience,
-            verbose= self.hparams.verbose,
-            mode= self.hparams.mode,
+            factor=self.hparams.factor,
+            patience=self.hparams.patience,
+            verbose=self.hparams.verbose,
+            mode=self.hparams.mode,
         )
-        
-        return [opt], [scheduler]
 
+        return [opt], [scheduler]
 
     def training_step(self, batch, batch_idx):
         audio, label = batch
@@ -54,23 +53,27 @@ class AutotaggingRunner(LightningModule):
         audio, label = batch
         prediction = self.model(audio)
         loss = self.criterion(prediction, label)
-        return {'val_loss': loss, 'predictions': prediction, 'labels': label}
-    
+        return {"val_loss": loss, "predictions": prediction, "labels": label}
+
     def validation_epoch_end(self, outputs):
         val_loss = torch.mean(torch.stack([output["val_loss"] for output in outputs]))
         predctions = torch.stack([output["predictions"] for output in outputs])
         labels = torch.stack([output["labels"] for output in outputs])
         roc_auc, pr_auc = get_auc(predctions, labels)
         return {
-            "progress_bar": {'val_loss': val_loss, 'roc_auc': roc_auc, 'pr_auc': pr_auc},
-            "log": {'val_loss': val_loss, 'roc_auc': roc_auc, 'pr_auc': pr_auc},
+            "progress_bar": {
+                "val_loss": val_loss,
+                "roc_auc": roc_auc,
+                "pr_auc": pr_auc,
+            },
+            "log": {"val_loss": val_loss, "roc_auc": roc_auc, "pr_auc": pr_auc},
         }
 
     def test_step(self, batch, batch_idx):
         audio, label = batch
         prediction = self.model(audio)
         loss = self.criterion(prediction, label)
-        return {'val_loss': loss, 'predictions': prediction, 'labels': label}
+        return {"val_loss": loss, "predictions": prediction, "labels": label}
 
     def test_epoch_end(self, outputs):
         val_loss = torch.mean(torch.stack([output["val_loss"] for output in outputs]))
@@ -78,5 +81,5 @@ class AutotaggingRunner(LightningModule):
         labels = torch.stack([output["labels"] for output in outputs])
         roc_auc, pr_auc = get_auc(predctions, labels)
         return {
-            "log": {'val_loss': val_loss, 'roc_auc': roc_auc, 'pr_auc': pr_auc},
+            "log": {"val_loss": val_loss, "roc_auc": roc_auc, "pr_auc": pr_auc},
         }
