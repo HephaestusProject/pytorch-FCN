bf29de2057b02aba2e4d9b728d2120ff74a55f96
Dohppak <seungheondoh@kaist.ac.kr>
2020-10-03 12:54:53 +0900

    feature:model implementation

--- src/model/net.py
+++ src/model/net.py
@@ -1,4 +1,140 @@
-"""
-    This script was made by Nick at 19/07/20.
-    To implement code of your network using operation from ops.py.
-"""
+# coding: utf-8
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchaudio
+from .ops import Conv_2d, Res_2d
+from torch.autograd import Variable
+
+
+class FCN(nn.Module):
+    '''
+    Choi et al. 2016
+    Automatic tagging using deep convolutional neural networks.
+    Fully convolutional network.
+    '''
+    def __init__(self,
+                sample_rate:int,
+                n_fft:int,
+                f_min:int,
+                f_max:int,
+                n_mels:int,
+                n_class:int):
+        super(FCN, self).__init__()
+
+        # Spectrogram
+        self.spec = torchaudio.transforms.MelSpectrogram(sample_rate=sample_rate,
+                                                         n_fft=n_fft,
+                                                         f_min=f_min,
+                                                         f_max=f_max,
+                                                         n_mels=n_mels)
+        self.to_db = torchaudio.transforms.AmplitudeToDB()
+        self.spec_bn = nn.BatchNorm2d(1)
+
+        # FCN
+        self.layer1 = Conv_2d(1, 64, pooling=(2,4))
+        self.layer2 = Conv_2d(64, 128, pooling=(2,4))
+        self.layer3 = Conv_2d(128, 128, pooling=(2,4))
+        self.layer4 = Conv_2d(128, 128, pooling=(3,5))
+        self.layer5 = Conv_2d(128, 64, pooling=(4,4))
+
+        # Dense
+        self.dense = nn.Linear(64, n_class)
+        self.dropout = nn.Dropout(0.5)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # Spectrogram
+        x = self.spec(x)
+        x = self.to_db(x)
+        x = x.unsqueeze(1)
+        x = self.spec_bn(x)
+
+        # FCN
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+
+        # Dense
+        x = x.view(x.size(0), -1)
+        x = self.dropout(x)
+        x = self.dense(x)
+        x = self.sigmoid(x)
+
+        return x
+
+class ShortChunkCNN_Res(nn.Module):
+    '''
+    Short-chunk CNN architecture with residual connections.
+    '''
+    def __init__(self,
+                n_channels: int,
+                sample_rate: int,
+                n_fft: int,
+                f_min: int,
+                f_max: int,
+                n_mels: int,
+                n_class: int
+                ):
+        super(ShortChunkCNN_Res, self).__init__()
+
+        # Spectrogram
+        self.spec = torchaudio.transforms.MelSpectrogram(sample_rate=sample_rate,
+                                                         n_fft=n_fft,
+                                                         f_min=f_min,
+                                                         f_max=f_max,
+                                                         n_mels=n_mels)
+        self.to_db = torchaudio.transforms.AmplitudeToDB()
+        self.spec_bn = nn.BatchNorm2d(1)
+
+        # CNN
+        self.layer1 = Res_2d(1, n_channels, stride=2)
+        self.layer2 = Res_2d(n_channels, n_channels, stride=2)
+        self.layer3 = Res_2d(n_channels, n_channels*2, stride=2)
+        self.layer4 = Res_2d(n_channels*2, n_channels*2, stride=2)
+        self.layer5 = Res_2d(n_channels*2, n_channels*2, stride=2)
+        self.layer6 = Res_2d(n_channels*2, n_channels*2, stride=2)
+        self.layer7 = Res_2d(n_channels*2, n_channels*4, stride=2)
+
+        # Dense
+        self.dense1 = nn.Linear(n_channels*4, n_channels*4)
+        self.bn = nn.BatchNorm1d(n_channels*4)
+        self.dense2 = nn.Linear(n_channels*4, n_class)
+        self.dropout = nn.Dropout(0.5)
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # Spectrogram
+        x = self.spec(x)
+        x = self.to_db(x)
+        x = x.unsqueeze(1)
+        x = self.spec_bn(x)
+
+        # CNN
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.layer6(x)
+        x = self.layer7(x)
+        x = x.squeeze(2)
+
+        # Global Max Pooling
+        if x.size(-1) != 1:
+            x = nn.MaxPool1d(x.size(-1))(x)
+        x = x.squeeze(2)
+
+        # Dense
+        x = self.dense1(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.dense2(x)
+        x = self.sigmoid(x)
+        return x
+
