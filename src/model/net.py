b84ff80733a6b92cf8f6ecda9e0c5509675ed0ae
GitHub Action <action@github.com>
2020-10-03 03:55:30 +0000

    style: isort/black

--- src/model/net.py
+++ src/model/net.py
@@ -4,40 +4,46 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchaudio
-from .ops import Conv_2d, Res_2d
 from torch.autograd import Variable
+
+from .ops import Conv_2d, Res_2d
 
 
 class FCN(nn.Module):
-    '''
+    """
     Choi et al. 2016
     Automatic tagging using deep convolutional neural networks.
     Fully convolutional network.
-    '''
-    def __init__(self,
-                sample_rate:int,
-                n_fft:int,
-                f_min:int,
-                f_max:int,
-                n_mels:int,
-                n_class:int):
+    """
+
+    def __init__(
+        self,
+        sample_rate: int,
+        n_fft: int,
+        f_min: int,
+        f_max: int,
+        n_mels: int,
+        n_class: int,
+    ):
         super(FCN, self).__init__()
 
         # Spectrogram
-        self.spec = torchaudio.transforms.MelSpectrogram(sample_rate=sample_rate,
-                                                         n_fft=n_fft,
-                                                         f_min=f_min,
-                                                         f_max=f_max,
-                                                         n_mels=n_mels)
+        self.spec = torchaudio.transforms.MelSpectrogram(
+            sample_rate=sample_rate,
+            n_fft=n_fft,
+            f_min=f_min,
+            f_max=f_max,
+            n_mels=n_mels,
+        )
         self.to_db = torchaudio.transforms.AmplitudeToDB()
         self.spec_bn = nn.BatchNorm2d(1)
 
         # FCN
-        self.layer1 = Conv_2d(1, 64, pooling=(2,4))
-        self.layer2 = Conv_2d(64, 128, pooling=(2,4))
-        self.layer3 = Conv_2d(128, 128, pooling=(2,4))
-        self.layer4 = Conv_2d(128, 128, pooling=(3,5))
-        self.layer5 = Conv_2d(128, 64, pooling=(4,4))
+        self.layer1 = Conv_2d(1, 64, pooling=(2, 4))
+        self.layer2 = Conv_2d(64, 128, pooling=(2, 4))
+        self.layer3 = Conv_2d(128, 128, pooling=(2, 4))
+        self.layer4 = Conv_2d(128, 128, pooling=(3, 5))
+        self.layer5 = Conv_2d(128, 64, pooling=(4, 4))
 
         # Dense
         self.dense = nn.Linear(64, n_class)
@@ -66,43 +72,48 @@ class FCN(nn.Module):
 
         return x
 
+
 class ShortChunkCNN_Res(nn.Module):
-    '''
+    """
     Short-chunk CNN architecture with residual connections.
-    '''
-    def __init__(self,
-                n_channels: int,
-                sample_rate: int,
-                n_fft: int,
-                f_min: int,
-                f_max: int,
-                n_mels: int,
-                n_class: int
-                ):
+    """
+
+    def __init__(
+        self,
+        n_channels: int,
+        sample_rate: int,
+        n_fft: int,
+        f_min: int,
+        f_max: int,
+        n_mels: int,
+        n_class: int,
+    ):
         super(ShortChunkCNN_Res, self).__init__()
 
         # Spectrogram
-        self.spec = torchaudio.transforms.MelSpectrogram(sample_rate=sample_rate,
-                                                         n_fft=n_fft,
-                                                         f_min=f_min,
-                                                         f_max=f_max,
-                                                         n_mels=n_mels)
+        self.spec = torchaudio.transforms.MelSpectrogram(
+            sample_rate=sample_rate,
+            n_fft=n_fft,
+            f_min=f_min,
+            f_max=f_max,
+            n_mels=n_mels,
+        )
         self.to_db = torchaudio.transforms.AmplitudeToDB()
         self.spec_bn = nn.BatchNorm2d(1)
 
         # CNN
         self.layer1 = Res_2d(1, n_channels, stride=2)
         self.layer2 = Res_2d(n_channels, n_channels, stride=2)
-        self.layer3 = Res_2d(n_channels, n_channels*2, stride=2)
-        self.layer4 = Res_2d(n_channels*2, n_channels*2, stride=2)
-        self.layer5 = Res_2d(n_channels*2, n_channels*2, stride=2)
-        self.layer6 = Res_2d(n_channels*2, n_channels*2, stride=2)
-        self.layer7 = Res_2d(n_channels*2, n_channels*4, stride=2)
+        self.layer3 = Res_2d(n_channels, n_channels * 2, stride=2)
+        self.layer4 = Res_2d(n_channels * 2, n_channels * 2, stride=2)
+        self.layer5 = Res_2d(n_channels * 2, n_channels * 2, stride=2)
+        self.layer6 = Res_2d(n_channels * 2, n_channels * 2, stride=2)
+        self.layer7 = Res_2d(n_channels * 2, n_channels * 4, stride=2)
 
         # Dense
-        self.dense1 = nn.Linear(n_channels*4, n_channels*4)
-        self.bn = nn.BatchNorm1d(n_channels*4)
-        self.dense2 = nn.Linear(n_channels*4, n_class)
+        self.dense1 = nn.Linear(n_channels * 4, n_channels * 4)
+        self.bn = nn.BatchNorm1d(n_channels * 4)
+        self.dense2 = nn.Linear(n_channels * 4, n_class)
         self.dropout = nn.Dropout(0.5)
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
@@ -137,4 +148,3 @@ class ShortChunkCNN_Res(nn.Module):
         x = self.dense2(x)
         x = self.sigmoid(x)
         return x
-
